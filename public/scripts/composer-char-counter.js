02171fd7d0f1951dbeaac823bfb0a31f9c4a4405
Henry Boyd <AmorphousCube@gmail.com>
2020-01-28 21:51:47 +0000

    added counter functionality

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,26 @@
+$(document).ready(function() {
+  const $tweet = $('main section form textarea')
+
+  $tweet.on('keydown', function() {
+    let textLength = $(this).val().length;
+    $(this).parent().children(".counter").html(140 - textLength);
+    if (textLength > 140){
+      $(this).parent().children(".counter").addClass("red");
+    } else {
+      $(this).parent().children(".counter").removeClass("red");
+
+    }
+  })
+
+  $tweet.on('keyup', function() {
+    let textLength = this.value.length;
+    $(this).parent().children(".counter").html(140 - textLength);
+    if (textLength > 140){
+      $(this).parent().children(".counter").addClass("red");
+    } else {
+      $(this).parent().children(".counter").removeClass("red");
+    }
+  })
+  
+  console.log("testing 1 2 3");
+});
